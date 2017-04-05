6b7485c968dd3a036cefb6c8ec9a6b0365baf445
Andrew Foghel <andrewfoghel@wireless145-056.wireless.wmich.edu>
2017-04-05 14:57:33 -0400

    added pods

---
+++ socialMediaTest/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialMediaTest
+//
+//  Created by Andrew Foghel on 4/1/17.
+//  Copyright © 2017 Andrew Foghel. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
